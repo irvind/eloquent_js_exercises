58a9f32140198acf8fbaaebc4dc6674f2ba663f5
Eugene Obukhov <irvind25@gmail.com>
2015-05-21 22:37:33 +0300

    animated world with creatures

--- chapter7/chapter7.js
+++ chapter7/chapter7.js
@@ -1,20 +1,34 @@
 var plan = [
-    "############################",
+    "############################", 
     "#      #    #      o      ##",
     "#                          #",
     "#          #####           #",
     "##         #   #    ##     #",
-    "###           ##     #     #",
-    "#           ###      #     #",
-    "#   ####                   #",
-    "#   ##       o             #",
-    "# o  #         o       ### #",
-    "#    #                     #",
-    "############################"
+    "###           ##     #     #",  
+    "#           ###      #     #", 
+    "#   ####                   #", 
+    "#   ##       o             #", 
+    "# o  #         o       ### #", 
+    "#    #                     #", 
+    "############################"  
 ];
 
+/*
+(function() {
+    var nonBlank = 0;
+    for (var i = 0; i < plan.length; i++) {
+        for (var j = 0; j < plan[0].length; j++) {
+            if (plan[i][j] != ' ')
+                nonBlank++;
+        }
+    }
+
+    console.log(nonBlank);
+})();
+*/
+
 function randomElement(array) {
-    return array[Math.random() * array.length];
+    return array[Math.floor(Math.random() * array.length)];
 }
 
 function Vector(x, y) {
@@ -42,6 +56,15 @@ Grid.prototype.get = function(vector) {
 };
 Grid.prototype.set = function(vector, value) {
     this.space[vector.x + (vector.y * this.width)] = value;
+};
+Grid.prototype.forEach = function(f, context) {
+    for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {
+            var value = this.space[x + y * this.width];
+            if (value != null) 
+                f.call(context, value, vec(x, y));
+        }
+    }
 };
 
 var directions = {
@@ -103,8 +126,71 @@ World.prototype.toString = function() {
 
     return output; 
 };
+World.prototype.turn = function() {
+    var acted = [];
+    this.grid.forEach(function(critter, vector) {
+        if (critter.act && acted.indexOf(critter) == -1) {
+            acted.push(critter);
+            this.letAct(critter, vector);
+        }
+    }, this);
+};
+World.prototype.letAct = function(critter, vector) {
+    var action = critter.act(new View(this, vector));
+    if (action && action.type == 'move') {
+        var dest = this.checkDestination(action, vector);
+        if (dest && this.grid.get(dest) == null) {
+            this.grid.set(vector, null);
+            this.grid.set(dest, critter);
+        }
+    }
+};
+World.prototype.checkDestination = function(action, vector) {
+    if (directions.hasOwnProperty(action.direction)) {
+        var dest = vector.plus(directions[action.direction]);
+        if (this.grid.isInside(dest)) 
+            return dest;
+    }
+};
 
 function Wall() {}
 
+function View(world, vector) {
+    this.world = world;
+    this.vector = vector;
+}
+View.prototype.look = function(dir) {
+    var target = this.vector.plus(directions[dir]);
+    if (this.world.grid.isInside(target)) 
+        return charFromElement(this.world.grid.get(target));
+    else
+        return '#';
+};
+View.prototype.findAll = function(ch) {
+    var found = [];
+    for (var dir in directions) {
+        if (this.look(dir) == ch)
+            found.push(dir);
+    }
+    return found;
+};
+View.prototype.find = function(ch) {
+    var found = this.findAll(ch);
+    if (found.length == 0) 
+        return null;
+    else
+        return randomElement(found);
+};
+
 var world = new World(plan, {'#': Wall, 'o': BouncingCritter});
-console.log(world.toString());
+
+animateWorld(world);
+
+// for (var i = 0; i < 5; i++) {
+//     try {
+//         world.turn();
+//     } catch (e) {
+//         console.log(e.stack);
+//     }
+//     console.log(world.toString());
+// }
