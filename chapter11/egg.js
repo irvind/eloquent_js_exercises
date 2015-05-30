48a75e0b76ed55b4acae0d9b6432fb028be184ec
Eugene Obukhov <irvind25@gmail.com>
2015-05-30 16:04:57 +0300

    chapter11 evalute func and special forms

--- chapter11/egg.js
+++ chapter11/egg.js
@@ -47,4 +47,70 @@ function parse(program) {
     return result.expr;
 }
 
+function evalute(expr, env) {
+    switch (expr.type) {
+        case 'value':
+            return expr.value;
+
+        case 'word':
+            if (expr.name in env)
+                return env[expr.name];
+            else 
+                throw new ReferenceError("Undefined variable: " + expr.name);
+
+        case 'apply':
+            if (expr.operator.type == 'word' && expr.operator.name in specialForms)
+                return specialForms[expr.operator.name](expr.args, env);
+
+            var op = evalute(expr.operator, env);
+            if (typeof op != 'function') 
+                throw new TypeError("Applying a non-function.");
+
+            return op.apply(null, expr.args.map(function(arg) {
+                return evalute(arg, env);
+            }));
+    }
+}
+
+var specialForms = Object.create(null);
+
+specialForms['if'] = function(args, env) {
+    if (args.length != 3)
+        throw new SyntaxError("Bad number of args to if");
+
+    if (evalute(args[0], env) !== false) 
+        return evalute(args[1], env);
+    else
+        return evalute(args[2], env);
+};
+
+specialForms['while'] = function(args, env) {
+    if (args.length != 2)
+        throw new SyntaxError("Bad number of args to while"); 
+
+    while (evalute(args[0], env) !== false) {
+        evalute(args[1], env);
+    }
+
+    return false;
+};
+
+specialForms['do'] = function(args, env) {
+    var value = false;
+    args.forEach(function(arg) {
+        value = evalute(arg, env);
+    });
+
+    return value;
+}
+
+specialForms['define'] = function(args, env) {
+    if (args.length != 2 && args[0].type !== 'word')
+        throw new SyntaxError("Bad use of define");
+
+    var value = evalute(args[1], env);
+    env[args[0].name] = value;
+    return value;
+};
+
  console.log(parse("+(a, 10)"));
